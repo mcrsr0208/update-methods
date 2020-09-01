2f64db92c749dc80f4559ebe32263a86e0affca1
mcrsr0208 <mcrsr0208@gmail.com>
2020-09-01 15:41:38 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-9np8zv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-9np8zv)
---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./style.css";
+
+class App extends React.Component{
+
+  constructor(props){
+    console.log('constructor()');
+    super(props);
+    this.state = {
+      data:'small'
+    }
+  }
+
+  handleClick = () =>{
+    console.log('handleClick()');
+    this.setState({
+      data:'BIG'
+    });
+  }
+
+  shouldComponentUpdate(){
+    console.log('shouldComponentUpdate()');
+    return false;
+  }
+
+  render(){
+    console.log('render()');
+    return(
+      <div>
+        <h1>{ this.state.data }</h1>
+        <button onClick={ this.handleClick }>Change Data</button>
+      </div>
+    );
+  }
+}
+
+export default App;
